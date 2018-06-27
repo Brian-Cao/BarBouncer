3b73e45c1774a66151bdf8b6b081eb37a3ca70c5
Brian Cao <brian.cao@icloud.com>
2018-06-27 15:18:55 -0700

    finished grid layout. about to move on to connecting level buttons to level scene

--- LevelCollectionCell.swift
+++ LevelCollectionCell.swift
@@ -13,11 +13,4 @@ class LevelCollectionCell: UICollectionViewCell {
 
     @IBOutlet weak var buttonView: UIButton!
     
-//    @IBAction func levelButtonTouchUpInside(_ sender: Any) {
-//        let level = Int((self.buttonView.titleLabel?.text)!)!
-//        selectedLevel = level
-//        print(level)
-//    }
-    
-
 }
--- ball to end/LevelSelectVC.swift
+++ ball to end/LevelSelectVC.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class LevelSelectVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        
+        layout.itemSize = CGSize(width: 40, height: 40)
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 5
+        let topGap = (collectionView.frame.height - 265)/32
+        let leftGap = (collectionView.frame.width - 220)/2
+        layout.sectionInset = UIEdgeInsets(top: topGap, left: leftGap, bottom: topGap, right: leftGap)
+        
+        layout.scrollDirection = .vertical
+        
+        collectionView.collectionViewLayout = layout
+        
+
+        
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 30
     }
@@ -21,12 +45,23 @@ class LevelSelectVC: UIViewController, UICollectionViewDelegate, UICollectionVie
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         // Setting each cell to be a "GridCell"
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LevelCell", for: indexPath) as! LevelCollectionCell
-        cell.buttonView.setTitle("\((indexPath.section * 30) + (indexPath.item + 1))", for: .normal)
+        let cellNumber = (indexPath.section * 30) + (indexPath.item + 1)
+        cell.buttonView.setTitle("\(cellNumber)", for: .normal)
+        cell.buttonView.tag = cellNumber
         
         return cell
     }
     
-
+    @IBAction func buttonTouched(_ sender: Any) {
+        let button = sender as! UIButton
+        print(Int((button.titleLabel?.text)!)!)
+        
+        // Move to Level Scene
+        selectedLevel = button.tag
+        self.performSegue(withIdentifier: "moveToLevelScene", sender: button)
+        
+    }
+    
 }
 
 
